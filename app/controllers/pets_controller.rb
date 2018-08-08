6586be39930abc4c43a4e384d72771bc620e89aa
Nancy Jo <enciie@gmail.com>
2018-08-08 22:31:42 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -35,7 +35,8 @@ class PetsController < ApplicationController
   end
 
   get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
+    binding.pry
+    # @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
 
